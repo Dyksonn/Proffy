26c3f2ba456f01c9c14cd5c702b43dd7244816b6
dykson <dykson@smit.net.br>
2020-08-10 16:14:08 -0300

    Finish aplicação web

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,34 +2,53 @@ import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
+import api from '../../services/api'
+
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    })
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/44515719?s=460&u=a1f723d87fd02ce964cabcdb6e2fac899a699f0d&v=4" alt="Dykson Santos" />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Dykson Santos</strong>
-          <span>Programação React Native</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Programação orientada a objetos, basico em javascript
-            <br /><br />
-            Apaixonado por tecnlogia desde de pequenininho e cada vez mais agora trabalhando na área apaixonado pela tecnologia
-          </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-              <strong>R$ 100,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a target="_black" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp" />
               Entrar em contato
-            </button>
+        </a>
       </footer>
     </article>
   )
